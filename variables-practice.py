0968e1e8c527205eafe2671b717bec0798e6602f
Amna shakil <hafizaaminarehman@gmail.com>
2025-05-09 23:59:24 +0500

    Add files via upload

---
+++ variables-practice.py
@@ -0,0 +1,15 @@
+name="Ali"
+age=20
+Course_name="Python_Basics"
+print(name)
+print(age)
+print(Course_name)
+#2name="Ali"  # Cannot start with a number
+#my-name="Ali"  # Dashes not allowed
+#user name="Ali" # Spaces not allowed
+student_name="shakeel"
+student_age= 18
+student_marks= 98
+print(student_name)
+print(student_age)
+print(student_marks)
